e4601ee2632185557300a6523b24e884ff8b0d1f
Shiyul4 <165734192+Shiyul4@users.noreply.github.com>
2025-05-06 14:53:05 -0500

    Update inventory.dart

--- SLB_Frontend/lib/inventory_files/inventory.dart
+++ SLB_Frontend/lib/inventory_files/inventory.dart
@@ -1,3 +1,4 @@
+
 import 'package:flutter/material.dart';
 import 'tabbed_inventory.dart';
 import 'dart:convert';
@@ -5,217 +6,159 @@ import 'dart:io';
 import 'package:http/http.dart' as http;
 import '../globals.dart' as global;
 
-Future<List<String>> getData() async {
+Future<List<String>> getUserParts() async {
   List<String> string_arr = [];
-  final String sendUrl = 'http://4.227.176.4:8081/api/categories';
+  final String sendUrl = 'http://4.227.176.4:8081/api/components';
   String token = global.token;
   print(token);
-  final response = await http.get(
-    Uri.parse(sendUrl),
-    headers: {'Authorization': token, 'Content-Type': 'application/json'},
-  );
-  //print(response.statusCode);
-  print(response.body);
-  if (response.statusCode == 200) {
-    List temp = jsonDecode(response.body);
-    print(temp);
-    //string_arr = temp.cast<String>();
-    for (int i = 0; i < temp.length; i++) {
-      string_arr.add(
-        "Product ID: ${temp[i]['productId']} \n In Stock: ${temp[i]['numberPartCheckOut']}",
-      );
-      print(temp[i]['productId']);
-      print(temp[i]);
-    }
-    //print(temp[2]);
-    //print(temp[3]);
-
-    //string_arr = response.body.split('\n');
-    //print(string_arr);
-    //print(string_arr.length);
-    //print(string_arr[3]);
-  } else {
-    print(response.statusCode);
-    throw Exception('Unable to connect');
-  }
-  //setState(() {});
-  return string_arr;
-}
-
-Future<List<String>> getnameData() async {
-  List<String> string_arr = [];
-  final uri = Uri.http('4.227.176.4:8081', '/api/categories', {
-    'name': 'Admin',
-  });
-  //final uri = Uri.http('172.191.111.81:8081', '/api/categories');
-  String token = global.token;
-  //print(token);
-  final response = await http.get(
-    uri,
-    headers: {'Authorization': token, 'Content-Type': 'application/json'},
-  );
-  //print(response.statusCode);
-  print(response.body);
-  if (response.statusCode == 200) {
-    List temp = List<String>.from(jsonDecode(response.body));
-    print(temp.length);
-    for (int i = 0; i < temp.length; i++) {
-      print(temp[i]);
-    }
-
-    //string_arr = response.body.split('\n');
-    //print(string_arr);
-    //print(string_arr.length);
-    //print(string_arr[3]);
-  } else {
-    print(response.statusCode);
-    throw Exception('Unable to connect');
-  }
-  //setState(() {});
-  return string_arr;
-}
-
-Future<void> sendData(String data) async {
-  //const string = 'temp temp temp';
-  //Map<String, String> headers = {"Content-Type": "application/json"};
-  final String sendUrl = 'http://4.227.176.4:8081/api/components';
-  final response = await http.post(
-    //Uri.parse('http://ptsv3.com/t/afeafaefaef/post/'),
+  final response = await http.get(  
     Uri.parse(sendUrl),
     headers: {
-      HttpHeaders.authorizationHeader:
-          'eyJhbGciOiJIUzI1NiJ9.eyJ1c2VyTmFtZSI6IkFkbWluIiwiZXhwIjoxNzQzMTE1NTE2fQ.6hlGpXVCPyCZdaeueBm98fdFC5jbRgT6TOZJUg7XJ1Q',
+      'Authorization': token,
       'Content-Type': 'application/json',
-    },
-    body: jsonEncode({'productsId': 0, 'category': 0, 'status': 'available'}),
+    }
   );
-  /*if(response.statusCode != 200) {
-    print(response.statusCode);
-    throw Exception('Unable to connect');
+  if(response.statusCode == 200) {
+    List temp =  jsonDecode(response.body);
+    for(int i = 0; i < temp.length; i++) {
+      string_arr.add("Part ID: ${temp[i]['partId']} \n Status: ${temp[i]['status']}");
+      //print(temp[i]);
+    }
   }
   else {
     print(response.statusCode);
+    throw Exception('Unable to connect');
+  }
+  return string_arr;
+}
+
+Future<List> getUserProducts() async {
+  //List<String> string_arr = [];
+  List items = [];
+  print("getting user products");
+  //final uri = Uri.http('172.191.111.81:8081', '/api/categories', {'name': 'Bob Lin'});
+  //print(uri);
+  final String sendUrl = 'http://4.227.176.4:8081/api/components/borrowed';
+  print(global.token);
+  final response = await http.get(  
+    Uri.parse(sendUrl),
+    //uri,
+    headers: {
+      'Authorization': global.token,
+      'Content-Type': 'application/json',
+    }
+  );
+  if(response.statusCode == 200) {
+    //print(response.body);
+    final responseBody = jsonDecode(response.body);
+    items = responseBody['data'];
+    //print(items);
+    //List temp =  jsonDecode(response.body);
+    //print(items);
+    /*for(int i = 0; i < items.length; i++) {
+      string_arr.add("Product ID: ${items[i]['productId']} \n"
+        "ProductName: ${items[i]['productName']} \n"
+       "Part ID: ${items[i]['partId']} \n"
+       "partName: ${items[i]['partName']}");
+      //print(temp[i]);
+    }*/
+  }
+  /*if(response.statusCode == 200) {
+    //print(response.body);
+    List temp =  jsonDecode(response.body);
+    for(int i = 0; i < temp.length; i++) {
+      string_arr.add("Product ID: ${temp[i]['productId']} \n"
+       "Number Available: ${temp[i]['numberPartInStock']} \n"
+       "Number Checked Out: ${temp[i]['numberPartCheckOut']}");
+      //print(temp[i]);
+    }
   }*/
-  print(response.statusCode);
+  else {
+    print(response.statusCode);
+    print(response.body);
+    //throw Exception('Unable to connect');
+  }
+  //return string_arr;
+  return items;
+}
+
+Future<void> borrowProduct() async {
+  print("borrowing product");
+  final String uri = 'http://4.227.176.4:8081//activities/borrow';
+  print(global.token);
+  final response = await http.post(  
+    Uri.parse(uri),
+    headers: {
+      'Authorization': global.token,
+      'Content-Type': 'application/json',
+    },
+    body: jsonEncode({
+      "part_id": 104
+    })
+  );
   print(response.body);
+  if(response.statusCode == 200) {
+    print(response.body);
+  }
 }
+/*Future<void> returnProduct(int id) async {
+  final String uri = 'http://172.191.111.81:8081/api/activities/borrow';
+  final response = await http.get(  
+    Uri.parse(uri),
+    //uri,
+    headers: {
+      //'name': 'Admin',
+      'Authorization': global.token,
+      'Content-Type': 'application/json',
+    }
+    body: {
+      
+    }
+  );
+  if(response.statusCode == 200) {
+    //print(response.body);
+    List temp =  jsonDecode(response.body);
+    for(int i = 0; i < temp.length; i++) {
+      string_arr.add("Product ID: ${temp[i]['productId']} \n"
+       "Number Available: ${temp[i]['numberPartInStock']} \n"
+       "Number Checked Out: ${temp[i]['numberPartCheckOut']}");
+      //print(temp[i]);
+    }
+  }
+  else {
+    print(response.statusCode);
+    print(response.body);
+    //throw Exception('Unable to connect');
+  }
 
-class InventoryPage extends StatefulWidget {
-  const InventoryPage({super.key});
-
-  @override
-  State<InventoryPage> createState() => _InventoryPageState();
-}
-
-class _InventoryPageState extends State<InventoryPage> {
-  List<String> history_arr = [];
-  bool logout = false;
-
-  @override
-  /*void initState() {
-    super.initState();
-    getData().then((List<String> result) {
-      history_arr = result;
-      setState(() {});
-    });
+}*/
+Future<void> deleteProduct(int id) async {
+  List<String> string_arr = [];
+  final uri = Uri.http('4.227.176.4:8081', '/api/categories', {'id': 1});
+  //final uri = Uri.http('172.191.111.81:8081', '/api/categories');
+  //final String sendUrl = 'http://172.191.111.81:8081/api/categories/$id';
+  String token = global.token;
+  //print(token);
+  /*final response = await http.delete(  
+    //Uri.parse(uri),
+    uri,
+    headers: {
+      'Authorization': token,
+      'Content-Type': 'application/json',
+    }
+  );
+  if(response.statusCode == 200) {
+    List temp =  jsonDecode(response.body);
+    for(int i = 0; i < temp.length; i++) {
+      string_arr.add("Product ID: ${temp[i]['productId']} \n"
+       "Number Available: ${temp[i]['numberPartInStock']} \n"
+       "Number Checked Out: ${temp[i]['numberPartCheckOut']}");
+      //print(temp[i]);
+    }
+  }
+  else {
+    print(response.statusCode);
+    throw Exception('Unable to connect');
   }*/
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      child: Column(
-        children: [
-          Container(
-            alignment: Alignment.center,
-            height: 500,
-            width: 500,
-            child: ListView.separated(
-              padding: const EdgeInsets.all(8),
-              itemCount: history_arr.length,
-              itemBuilder: (BuildContext context, int index) {
-                return InventoryTile(tileString: history_arr[index]);
-              },
-              separatorBuilder:
-                  (BuildContext context, int index) => const Divider(),
-            ),
-          ),
-          FloatingActionButton(
-            onPressed: () {
-              //Navigator.push(context, MaterialPageRoute(builder: (context) => const MyTabbedPage()));
-            },
-            child: Text('tabbed inventory'),
-          ),
-          FloatingActionButton(
-            heroTag: null,
-            onPressed: () async {
-              history_arr = await getData();
-              setState(() {});
-            },
-            child: Text("get Data"),
-          ),
-          FloatingActionButton(
-            heroTag: null,
-            onPressed: () {
-              sendData("temp");
-            },
-            child: Text('Send Data'),
-          ),
-          FloatingActionButton(
-            heroTag: null,
-            onPressed: () {
-              getnameData();
-            },
-            child: Text('get name Data'),
-          ),
-        ],
-      ),
-    );
-  }
-}
-
-class InventoryTile extends StatefulWidget {
-  final String tileString;
-  const InventoryTile({super.key, required this.tileString});
-
-  @override
-  State<InventoryTile> createState() => _InventoryTileState();
-}
-
-class _InventoryTileState extends State<InventoryTile> {
-  //final String tileString;
-  bool logout = false;
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      height: 75,
-      width: 500,
-      child: Card(
-        //height: 50,
-        //color: Colors.amber[colorCodes[index]],
-        clipBehavior: Clip.hardEdge,
-        child: InkWell(
-          splashColor: Colors.blue.withAlpha(30),
-          onTap: () {
-            logout = !logout;
-            setState(() {});
-          },
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              Text(widget.tileString),
-              //Text('Entry ${history_arr[index]}'),
-              logout
-                  ? FloatingActionButton(
-                    onPressed: () {
-                      sendData(widget.tileString);
-                    },
-                    child: Text('Checkout'),
-                  )
-                  : Container(),
-            ],
-          ),
-        ),
-      ),
-    );
-  }
+  //return string_arr;
 }

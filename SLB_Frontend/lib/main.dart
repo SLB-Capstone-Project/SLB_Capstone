89c2320f7242693863f47b5b2e286f50bfd24642
Jessica Hsiao <136421644+jh1231223@users.noreply.github.com>
2025-05-05 23:35:06 -0500

    commit

--- SLB_Frontend/lib/main.dart
+++ SLB_Frontend/lib/main.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
-//import 'home_page.dart';
-import 'login_files/login_screen.dart';
+import 'home_page.dart';
+//import 'login_files/login_screen.dart';
 //import 'camera_page.dart';
-
 
 void main() {
   runApp(const MyApp());
@@ -13,7 +12,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(title: 'SLB APP', home: LoginScreen(),);
-    //return MaterialApp(title: 'SLB APP', home: HomePage(),);
+    //return MaterialApp(title: 'SLB APP', home: LoginScreen(),);
+    return MaterialApp(title: 'SLB APP', home: HomePage());
   }
 }

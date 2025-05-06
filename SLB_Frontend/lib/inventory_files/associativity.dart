fabd2c9a329edd89e048ae06a9903e864f43b65b
Allen Peng <allenpeng@Erics-MacBook-Pro-2.local>
2025-05-06 15:43:35 -0500

    new home page changes

---
+++ SLB_Frontend/lib/inventory_files/associativity.dart
@@ -0,0 +1,97 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'provider.dart';
+
+class SPartList extends StatefulWidget {
+  final int? product;
+  final List<int> partList;
+  SPartList({super.key, required this.product, required this.partList,});
+
+  @override
+  _SPartListState createState() => _SPartListState();
+}
+
+class _SPartListState extends State<SPartList> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Parts for Product ${widget.product}'),
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: SizedBox(
+              height: 200,
+              child: ListView.separated(
+                padding: const EdgeInsets.all(8),
+                itemCount: widget.partList.length,
+                itemBuilder: (BuildContext context, int index) {
+                    return Consumer<UserProducts>( 
+                      builder: (context, products, child) {
+                        return ListTile(
+                          leading: Text(widget.partList[index].toString()),
+                          onTap: () {
+                              int part = widget.partList[index];
+                              List<int>? partIdList = products.getPartIdList(widget.product ?? 0, part);
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(builder: (context) => PartList(partList: partIdList ?? [])),
+                              );
+                            }
+                        );
+                      },
+                    );
+                },
+                separatorBuilder: (BuildContext context, int index) => const Divider(),
+              ),
+            ),
+          ), 
+        ]
+      )
+    );
+  }
+}
+
+class PartList extends StatefulWidget {
+  final List<int> partList;
+  PartList({super.key, required this.partList});
+
+  @override
+  _PartListState createState() => _PartListState();
+}
+
+class _PartListState extends State<PartList> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Part ID'),
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: SizedBox(
+              height: 200,
+              child: ListView.separated(
+                padding: const EdgeInsets.all(8),
+                itemCount: widget.partList.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return ListTile(
+                    leading: Text(widget.partList[index].toString()),
+                    onTap: () {
+                      int part = widget.partList[index];
+                      print(part);
+                      }
+                    //onTap: () => {remove_index(index)},
+                  );
+                },
+                separatorBuilder: (BuildContext context, int index) => const Divider(),
+              ),
+            ),
+          ), 
+        ]
+      )
+    );
+  }
+}
---
+++ SLB_Frontend/lib/inventory_files/windows/partIdList.dart
@@ -0,0 +1,153 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import '../provider.dart';
+
+
+class PartIdList extends StatelessWidget {
+  final bool specific;
+  final int? product;
+  final int? part;
+  const PartIdList({super.key, required this.product, required this.part, required this.specific});
+
+  @override
+  Widget build(BuildContext context) {
+    List<int> partIdList; 
+    if(specific) {
+      partIdList = Provider.of<UserProducts>(context, listen: false).getPartIdList(product ?? 0, part ?? 0) ?? [];
+    }
+    else {
+      partIdList = Provider.of<UserProducts>(context, listen: false).getAllPartIds;
+    }
+    //List<int> partIds = Provider.of<UserProducts>(context, listen: false).getPartIds; 
+    return Material(child: Container(
+      color: const Color.fromRGBO(10, 10, 10, 1),
+      child: Column (
+        children: [
+          if(specific) AppBar(
+            title: Text('Part Ids for product/part $product$part'),
+            backgroundColor: const Color.fromRGBO(38, 38, 50, 1),
+            toolbarHeight: 56.0,
+            titleTextStyle: const TextStyle(
+              fontSize: 20.0,
+              fontWeight: FontWeight.bold,
+              color:  Color.fromRGBO(240, 240, 240, 1),
+            ),
+            /*leading: IconButton(  
+              icon: const Icon(Icons.home),
+              color: const Color.fromRGBO(240, 240, 240, 1),
+              onPressed: () {
+                Navigator.pop(context);
+              }
+            ),*/
+          ),
+          Flexible(
+            child: CustomScrollView(
+              slivers: <Widget>[
+                SliverList(
+                  delegate: SliverChildBuilderDelegate((BuildContext context, int index) {
+                      int partId = partIdList[index];
+                      /*return Card( 
+                        child: ListTile(  
+                          /*leading: CircleAvatar( 
+                            foregroundColor: const Color.fromRGBO(240, 240, 240, 1),
+                            backgroundColor: const Color.fromRGBO(60, 61, 55, 1),
+                            child: Text(
+                              formattedText[0].substring(0, 3),
+                              //selectionColor: Color.fromRGBO(240, 240, 240, 1),
+                            ),
+                          ),*/
+                        
+                          tileColor:const Color.fromRGBO(22, 22, 30, 1),
+                          title: Text("Part ID: $partId"),
+                          //subtitle: Text(formattedText[1]),
+                          textColor: const Color.fromRGBO(240, 240, 240, 1),
+                          onTap: () {
+                          // List<int> partList = Provider.of<UserProducts>(context, listen: false).getPartList(productId) ?? [];
+                          // Navigator.push(
+                          //   context,
+                            //  MaterialPageRoute(builder: (context) => SPartList(product: productId, partList: partList)),
+                            //);
+                          }
+                        ),
+                      );*/
+                      return Padding(
+                          padding: EdgeInsets.only( 
+                            bottom: 12.0
+                          ),
+                          child: Container( 
+                          height: 48.0,
+                          width: double.infinity,
+                          decoration: BoxDecoration(borderRadius: BorderRadius.circular(8.0), color:const Color.fromRGBO(22, 22, 30, 1)),
+                          //color: const Color.fromRGBO(22, 22, 30, 1),
+                          child: Row(  
+                            children: [  
+                              Padding(  
+                                padding: EdgeInsets.all(12.0),
+                                child: Container( 
+                                  height: double.infinity,
+                                  width: 100,
+                                  decoration: BoxDecoration(borderRadius: BorderRadius.circular(16.0), color: const Color.fromRGBO(38, 38, 50, 1)),
+                                  //color: const Color.fromRGBO(38, 38, 50, 1),
+                                  child: Align( 
+                                    alignment: Alignment.center,
+                                    child: Text(  
+                                  style: TextStyle(fontFamily: 'RobotoMono', fontSize: 14, fontWeight: FontWeight.bold, color: const Color.fromRGBO(240, 240, 240, 1)), 
+                                  "$partId"
+                                  ))
+                                  
+                                ),
+                              ),
+                              /*Padding( 
+                                padding: const EdgeInsets.only(top: 8.0),
+                                child: Column(  
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [  
+                                  Text(  
+                                    style: TextStyle(fontFamily: 'RobotoMono', fontSize: 16, color: const Color.fromRGBO(240, 240, 240, 1)), 
+                                    "Part Name $index"
+                                  ),
+                                  /*Text(  
+                                    style: TextStyle(fontFamily: 'RobotoMono', fontSize: 12, color: const Color.fromRGBO(240, 240, 240, 1)), 
+                                    " parts"
+                                  )*/
+                                ]
+                              )),*/
+                              Spacer(),
+                              Align(
+                                alignment: Alignment.topCenter,
+                                /*child: Padding(
+                                  padding: EdgeInsets.all(8.0),
+                                  child: RichText(
+                                    text: TextSpan( 
+                                      text: "View All",
+                                      style: TextStyle(
+                                        fontSize: 12, 
+                                        color: const Color.fromRGBO(240, 240, 240, 1)
+                                      ),
+                                      /*recognizer: TapGestureRecognizer()..onTap = () {
+                                        Navigator.push(context, MaterialPageRoute(
+                                          builder: (context) => PartIdList(product: product, part: part, specific:true)),
+                                        );
+                                      }*/
+                                    ),
+                                  )
+                                )*/
+                              )
+
+                            ]
+                          )
+                          )
+                      );
+                    }, 
+                    childCount: partIdList.length,
+                  ),                 
+                ),
+              ],
+            )
+          )
+        ]
+      )
+    ));
+  }
+}
+
